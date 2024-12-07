045b4d329312067c0de95e0a2cd65218f86fe153
sahilbatra1211 <sahilbatra1211@gmail.com>
2024-12-08 00:42:15 +0530

    push front end code

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Header = () => {
+    return (
+        <div className='flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20 '>
+
+            {/* --------- Header Left --------- */}
+            <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
+            <p className='text-2xl md:text-3xl lg:text-4xl text-white font-semibold leading-snug md:leading-snug lg:leading-snug'>
+             Stuck on a DSA Problem? <br /> Book Expert Coder to Solve It 
+             <span className='text-yellow-400'> Instantly!</span>
+            </p>
+                <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
+                    <img className='w-28' src={assets.group_profiles} alt="" />
+                    <p>Simply book a coder for an hour send him some questions<br className='hidden sm:block' /> he'll send you back hand written solutions with no plague instantly</p>
+                </div>
+                <a href='#speciality' className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-[#595959] text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>
+                    Book appointment <img className='w-3' src={assets.arrow_icon} alt="" />
+                </a>
+            </div>
+
+            {/* --------- Header Right --------- */}
+            <div className='md:w-1/2 relative'>
+                <img className='w-full md:absolute bottom-0 h-auto rounded-lg' src={assets.header_img} alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Header
