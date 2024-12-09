433928992705ac003504469271d6f55d4446b9dc
sahilbatra1211 <sahilbatra1211@gmail.com>
2024-12-10 00:42:40 +0530

    part 3

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -23,19 +23,19 @@ const Doctors = () => {
   useEffect(() => {
     applyFilter()
   }, [coders, speciality])
-
+// so the point is these needs to be changed general physician and all but can only be done once you have changed it in the backend majoryly admin portal
   return (
     <div>
-      <p className='text-gray-600'>Browse through all Specialist Coders</p>
+      <p className='text-black text-lg'>Browse through all Specialist Coders</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
         <button onClick={() => setShowFilter(!showFilter)} className={`py-1 px-3 border rounded text-sm  transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ''}`}>Filters</button>
         <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex' : 'hidden sm:flex'}`}>
-          <p onClick={() => speciality === 'General physician' ? navigate('/coders') : navigate('/coders/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'General physician' ? 'bg-[#E2E5FF] text-black ' : ''}`}>General physician</p>
-          <p onClick={() => speciality === 'Gynecologist' ? navigate('/coders') : navigate('/coders/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Gynecologist' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Gynecologist</p>
-          <p onClick={() => speciality === 'Dermatologist' ? navigate('/coders') : navigate('/coders/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Dermatologist' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Dermatologist</p>
-          <p onClick={() => speciality === 'Pediatricians' ? navigate('/coders') : navigate('/coders/Pediatricians')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Pediatricians' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Pediatricians</p>
-          <p onClick={() => speciality === 'Neurologist' ? navigate('/coders') : navigate('/coders/Neurologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Neurologist' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Neurologist</p>
-          <p onClick={() => speciality === 'Gastroenterologist' ? navigate('/coders') : navigate('/coders/Gastroenterologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Gastroenterologist' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Gastroenterologist</p>
+          <p onClick={() => speciality === 'General physician' ? navigate('/coders') : navigate('/coders/C++ DSA Experts')} className={`whitespace-nowrap w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'General physician' ? 'bg-[#E2E5FF] text-black ' : ''}`}>C++ DSA Experts</p>
+          <p onClick={() => speciality === 'Gynecologist' ? navigate('/coders') : navigate('/coders/Gynecologist')} className={`whitespace-nowrap w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Gynecologist' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Java DSA Experts</p>
+          <p onClick={() => speciality === 'Dermatologist' ? navigate('/coders') : navigate('/coders/Dermatologist')} className={`whitespace-nowrap w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Dermatologist' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Python DSA Experts</p>
+          <p onClick={() => speciality === 'Pediatricians' ? navigate('/coders') : navigate('/coders/Pediatricians')} className={`whitespace-nowrap w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Pediatricians' ? 'bg-[#E2E5FF] text-black ' : ''}`}>LLD Experts</p>
+          <p onClick={() => speciality === 'Neurologist' ? navigate('/coders') : navigate('/coders/Neurologist')} className={`whitespace-nowrap w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Neurologist' ? 'bg-[#E2E5FF] text-black ' : ''}`}>HLD Experts</p>
+          <p onClick={() => speciality === 'Gastroenterologist' ? navigate('/coders') : navigate('/coders/Gastroenterologist')} className={`whitespace-nowrap w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Gastroenterologist' ? 'bg-[#E2E5FF] text-black ' : ''}`}>Aptitude Experts</p>
         </div>
         <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
           {filterDoc.map((item, index) => (
