badb27c47552813b534696744b7cf0f207f001aa
sahilbatra1211 <sahilbatra1211@gmail.com>
2025-01-04 14:42:45 +0530

    Add Interview Experiences component

---
+++ src/pages/InterviewPrep.jsx
@@ -0,0 +1,10 @@
+const InterviewPrep = () => {
+    return (
+      <div>
+        <h1>Interview Preparation</h1>
+        <p>Welcome to the Interview Prep section! Explore patterns, tips, and resources.</p>
+      </div>
+    );
+  };
+  
+  export default InterviewPrep;
